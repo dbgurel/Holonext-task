6aefe910e6702acd4f8f4be13fada4932eadecef
dbgurel <berkingurel@gmail.com>
2022-01-15 16:00:17 +0300

    Slider component was updated. Images & description were added. Menu was created.

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,36 +1,69 @@
 import React, { useState } from 'react'
 import { SliderData } from './SliderData'
 import Logo from './images/holonext-logo.png'
-import { FaArrowLeft, FaArrowRight } from 'react-icons/fa'
+import { FaArrowLeft, FaArrowRight, FaBars } from 'react-icons/fa'
 
 const Slider = ({ slides }) => {
     const [current, setCurrent] = useState(0)
     const length = slides.length
+    const [toggleStatus, setToggleStatus] = useState(false)
 
     const prevSlide = () => {
         setCurrent(current === 0 ? length - 1 : current - 1)
+        console.log(current);
     }
     const nextSlide = () => {
         setCurrent(current === length - 1 ? 0 : current + 1)
     }
-
+    const setToggle = () => {
+        setToggleStatus(!toggleStatus)
+    }
+    const transformModel = (item) => {
+        let modelId = item.sceneId
+        alert(modelId)
+    }
     return (
-        <section className='mainContainer'>
-            <div className='title'>
+        <div className='mainContainer'>
+            <section className='title'>
+                <a href='https://holonext.com'><img src={Logo} className='title-logo' /></a>
+            </section>
+            <section className='slider' >
+                <div className='arrows'>
+                    <FaArrowLeft className='left-arrow' onClick={() => { prevSlide() }} />
+                    <FaArrowRight className='right-arrow' onClick={() => { nextSlide() }} />
+                </div>
+                <div className='models'>
+                    {SliderData.map((item, index) => {
+                        return (
+                            <div className='imageContainer' key={index}>
 
-                <a href='httphttps://holonext.com' > <img src={Logo} alt='logo' className='headerLogo headerLogo-s' /></a>
-            </div>
-            <div className='contentContainer'>
-                <FaArrowLeft className='left-arrow' onClick={prevSlide()} />
-                <FaArrowRight className='right-arrow' onClick={nextSlide()} />
-                {SliderData.map((item, index) => {
-                    return (<img src={item.image} alt='' className='image'/>)
-                })}
+                                {index === current && (<>
+                                    <img src={item.image} className='images' alt='' />
+                                    <p className='description'> {item.description} </p>
+                                    <button onClick={() => { transformModel(item) }} className='btn' > 3D Olarak Görüntüle </button>
+                                </>
+                                )}
 
-            </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </section>
+            <section className='alt-menu'>
 
+                <div className='menuContent'>
+                    <span className='menu-item'>Home</span>
+                    <span className='menu-item'>Models</span>
+                    <span className='menu-item'>Contact</span>
+                    <span className='menu-item'>Information</span>
+                    <span className='menu-item'>Etc.</span>
+                </div>
+                <div className='menu-icon'>
+                    <FaBars onClick={() => { setToggle() }} className='menu-icon' />
+                </div>
+            </section>
 
-        </section>
+        </div>
     )
 }
 
